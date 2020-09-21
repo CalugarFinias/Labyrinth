8435fb2dc8ba3de8d63163f301a49cdd6f1dbfee
rob <195286@ecam.be>
2020-09-21 14:00:22 +0200

    modif du code capteurUS

--- Code arduino/capteurUS/capteurUS.ino
+++ Code arduino/capteurUS/capteurUS.ino
@@ -40,12 +40,12 @@ double distanceComputation(USsensor *usSensor){
   pulseDuration = pulseIn(usSensor->echoPin, HIGH);  // retourne des micro secondes
   distanceInCM = (pulseDuration*340/2)/pow(10,4);   // pow(nombre, exposant) -> transforme en cm
 
-  // Test permettant de vérifier si la mesure prise est correcte ou pas (pas trop différente de la précédente).
+  // Test permettant de vérifier si la mesure prise est correcte ou pas (si elle est pas trop différente de la précédente).
   //    - La première comparaison permet de vérifier si la valeur actuel n'est pas trop différente de la
   //      précédente (ici 5x, à choisir) car il y a peu de chance que le robot passe de 40cm à 200cm en 1s.
   //    - La deuxième comparaison permet de s'assurer de ne pas prendre en compte la variable inisialisé (=0).
   //    - La troisième s'assure qu'on est pas dans des distances trop courte car 5x 2cm = 10cm => ce qui n'est pas 
-  //      impossible vu la courte distance.
+  //      impossible que le robot fasse comme distance vu la courte distance.
   if((distanceInCM > DISTANCE_ERROR*usSensor->lastDistance) && (usSensor->lastDistance != 0) && (usSensor->lastDistance > 10.0))
     distanceInCM = usSensor->lastDistance;
 
@@ -57,5 +57,4 @@ void displayDistance(USsensor *usSensor){
   double distance = 0;
   distance = distanceComputation(usSensor);
   Serial.println(distance);
-  //Serial.println();
 }
