f447693d58171f1e84fc3b85f46c6facd3c3cf0a
CVandermies <charlesvdm@outlook.com>
2020-10-09 05:16:21 +0200

    sketch different  cases

--- Code_raspberry/commonOperation.py
+++ Code_raspberry/commonOperation.py
@@ -3,6 +3,7 @@ from robotControl import *
 
 # valeur à partir de laquelle on considère qu'il n'y a pas de chemin et donc un mur.
 wallDistance = 10
+wallFront = 5
 
 
 def getDataFromArduino(dataType):
@@ -25,7 +26,7 @@ def commandMotors(wheelLeft, wheelRight, servo):
     #wait(2)
 
 
-
+"""
 def culDeSacSituation():
     n = Utility.noChange
     # informe s'il y a un chemin sur la droite = 1, ou pas = 0.
@@ -40,8 +41,41 @@ def culDeSacSituation():
     # on tourne le servo à gauche (0° étant le repère devant, +1 = 90° à droite et -1 = 90° à gauche).
     commandMotors(0,0,-1)
     if (getDataFromArduino(Utility.dataUsSensorOnServo) > wallDistance):
-        pathOnRight = 1
+        pathOnLeft = 1
 
     return [pathOnRight, pathOnLeft]
+"""
+
+n = Utility.noChange
+pathOnRight = 0
+pathOnLeft = 0
+commandMotors(0,0,0)
+getR = getDataFromArduino(Utility.dataRightUsSensor)
+getLF = getDataFromArduino(Utility.dataUsSensorOnServo)
+
+while True:
+    
+    #il faut définir une conversion de valeur
+    if (getR <= 1):
+        commandMotors(0,1,n))
+    elif (getR > wallDistance and getLF > wallFront):
+        commandMotors(2,1,n)
+    elif (getR < wallDistance and getLF < wallFront):
+        commandMotors(n,n,-1)
+        #si chemin à gauche
+        if (getLF > wallDistance):
+            pathOnLeft = 1
+            commandMotors(0,3,0)
+        #demi tour    
+        else :
+            pathOnLeft = 0
+            commandMotors(0,6,0)
+    #avance dans tous les cas
+    else :
+        pathOnRight = 0
+        commandMotors(1,1,n)
+
+
+
 
 
