0badbeecb4b0de592c0d0862ea8a8750d7cc1269
plopsdev <jonathan.miel@outlook.com>
2020-11-15 09:57:21 +0100

    fix multiple sensors serie com

--- Communication série/mecatronic_serie/mecatronic_serie.ino
+++ Communication série/mecatronic_serie/mecatronic_serie.ino
@@ -1,45 +1,85 @@
-const int echoPin = 2; // Trigger Pin of Ultrasonic Sensor
-const int trigPin = 3; // Echo Pin of Ultrasonic Sensor
+const int echoPinLeft = 2; // Trigger Pin of Left Ultrasonic Sensor
+const int trigPinLeft = 3; // Echo Pin of Left Ultrasonic Sensor
+const int echoPinRight = 4; // Trigger Pin of Right Ultrasonic Sensor
+const int trigPinRight = 5; // Echo Pin of Right Ultrasonic Sensor
 
-long duration;
-int distance;
+String new_inner_command ="";
+
+String left_to_pi;
+String right_to_pi;
+String data_to_pi;
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinLeft, OUTPUT);
+  pinMode(echoPinLeft, INPUT);
+  pinMode(trigPinRight, OUTPUT);
+  pinMode(echoPinRight, INPUT);
 }
 
 void loop() {
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.println(distance);
-  delay(100);
-  if(Serial.available()>0){
-    Serial.println(distance);
-    delay(100);  
-  }
+  left_to_pi = readLeftCaptor();
+  right_to_pi = readRightCaptor();
+  data_to_pi = left_to_pi + right_to_pi;
+  Serial.println(data_to_pi);
+  delay(200);
+  new_inner_command = get_serial_command_from_pi();
+  messageDecode(new_inner_command);
   
- /* if(Serial.available()>0){
-    char fromPi = Serial.read();
-    Serial.print("I received: ");
-    Serial.println(fromPi);
-    if(fromPi = 'E'){
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000);
-      digitalWrite(LED_BUILTIN,LOW);
-      delay(1000); 
+}
+
+String readLeftCaptor() {
+  long duration;
+  int distance_left;
+  String left="left ";
+  String space = " "; 
+  
+  digitalWrite(trigPinLeft, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinLeft, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinLeft, LOW);
+  duration = pulseIn(echoPinLeft, HIGH);
+  distance_left = duration * 0.034 / 2; 
+  left = left + String(distance_left);
+  left = left + space;
+  return left;
+}
+
+String readRightCaptor() {
+  long duration;
+  int distance_right;
+  String right = "right ";
+  
+  digitalWrite(trigPinRight, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinRight, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinRight, LOW);
+  duration = pulseIn(echoPinRight, HIGH);
+  distance_right = duration * 0.034 / 2; 
+  right = right + String(distance_right);
+  return right;
+}
+
+String get_serial_command_from_pi(){
+  String inner_message ;
+  if(Serial.available()>0){
+    while(Serial.available()>0){
+      char in_char = Serial.read();
+      inner_message +=in_char;
+      delay(2);
+      }
     }
-  }*/
+  return inner_message;
+}
+
+void messageDecode(String command){
+  if (command == "a"){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
--- Communication série/testconnection.py
+++ Communication série/testconnection.py
@@ -1,20 +1,36 @@
 import serial
+import re
+import time
 
 periph = '/dev/ttyACM0'
-
 baudrate = 9600
-
 ser = serial.Serial(periph, baudrate)
-toArduino='E'
-toArduinoEncode = toArduino.encode() #traduction en bytes pour passer par le canal série
+to_arduino_a= 'a'
+to_arduino_b= 'b'
+maximum_distance = 15
+distance_left=15
+distance_right=15
 
-maximumDistance = 30
-# def proximityFilter(distance) :
-
+def proximityTrigger(distance_left, distance_right) :
+    #print(distance_left)
+    #print(distance_right)
+    if distance_left <= 6 and distance_right <=6 :
+        print("Allumé")
+        ser.write(to_arduino_a.encode())      
+    else:
+        print("Eteint")
+        ser.write(to_arduino_b.encode())      
+        
 
 while True:
-    if(ser.in_waiting > 0):
-        line=ser.readline().decode('utf-8').rstrip() 
-        if int(line) < maximumDistance:
-            print(line) #print ce qu'il recoit du Arduino
-        ser.write(toArduinoEncode) #envoie 'E' en bytes à l'arduino
+    while ser.in_waiting:
+        line=ser.readline().decode('utf-8').rstrip()
+        print(line)
+        #if len(line)>1: #if this is not a or b (message from the pi to the arduino)
+        line_split=line.split()
+        if line_split[0]=='left' and int(line_split[1]) < maximum_distance:
+            distance_left=int(line_split[1])
+        if line_split[2]=='right' and int(line_split[3]) < maximum_distance:
+            distance_right=int(line_split[1])
+        proximityTrigger(distance_left, distannce_right)
+        #time.sleep(0.2)
